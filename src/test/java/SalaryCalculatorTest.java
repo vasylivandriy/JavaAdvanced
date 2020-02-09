a604fc82007ffa9b64a3f3be0f042486bba684bb
vasylivandriy <vasylivandriy@gmail.com>
2020-02-10 01:59:48 +0200

    1 commit

---
+++ src/test/java/SalaryCalculatorTest.java
@@ -0,0 +1,42 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class SalaryCalculatorTest {
+
+    private Main main;
+
+
+    @Before
+    public void setup() {
+
+        main = new Main();
+    }
+
+
+    @Test
+    public void salaryTest() {
+
+        int actual = main.salary(main.salaryCalculator0.getSalaryPerHour(), main.salaryCalculator0.getHoursWork(), main.salaryCalculator0.getHoursHospital(),main.salaryCalculator0.getHourHospitalSalary());
+        int expected = 20200;
+
+        Assert.assertEquals("It is not true", expected, actual);
+    }
+
+
+
+    @Test
+    public void hospitalSalaryTest() throws IOException {
+
+        main.salaryCalculator5.setHoursHospital(10);
+
+        int actual =  main.hospitalSalary(main.salaryCalculator5.getHoursHospital(),main.salaryCalculator5.getHourHospitalSalary());
+        int expected = 1500;
+        Assert.assertEquals(expected,actual);
+        Assert.assertTrue(actual>1000);
+    }
+
+
+}
