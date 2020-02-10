61e8329da3c6e05ed319205ff54201c9567efd99
vasylivandriy <vasylivandriy@gmail.com>
2020-02-10 15:15:48 +0200

    2 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
     SalaryCalculator salaryCalculator2 = new SalaryCalculator("Oleh", "Serif", 50, 172, 9, 25);
     SalaryCalculator salaryCalculator3 = new SalaryCalculator("Karl", "Aref", 150, 172, 12, 75);
     SalaryCalculator salaryCalculator4 = new SalaryCalculator("Karolina", "Vagen", 100, 172, 0, 50);
-    SalaryCalculator salaryCalculator5 = new SalaryCalculator("Pavlo", "Agen", 300, 172, 72, 150);
+    SalaryCalculator salaryCalculator5 = new SalaryCalculator("Pavlo", "Agen", 300, 172, 150, 150);
 
 
     int salary(int salaryPerHour, int hoursWork, int hoursHospital, int hourHospitalSalary) {
@@ -27,5 +27,20 @@ public class Main {
         }
     }
 
+
+    boolean isWorkSalary (int workSalary){
+
+        if (workSalary<20000){
+            try {
+                throw new Exception("Hospital Salary can't be more then work salary. Worker was sicken too much time!!!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+       return Boolean.parseBoolean(null);
+        }
+        else return workSalary>20000;
+
+
+    }
 
 }
--- src/test/java/SalaryCalculatorTest.java
+++ src/test/java/SalaryCalculatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -19,12 +20,11 @@ public class SalaryCalculatorTest {
     @Test
     public void salaryTest() {
 
-        int actual = main.salary(main.salaryCalculator0.getSalaryPerHour(), main.salaryCalculator0.getHoursWork(), main.salaryCalculator0.getHoursHospital(),main.salaryCalculator0.getHourHospitalSalary());
+        int actual = main.salary(main.salaryCalculator0.getSalaryPerHour(), main.salaryCalculator0.getHoursWork(), main.salaryCalculator0.getHoursHospital(), main.salaryCalculator0.getHourHospitalSalary());
         int expected = 20200;
 
         Assert.assertEquals("It is not true", expected, actual);
     }
-
 
 
     @Test
@@ -32,10 +32,20 @@ public class SalaryCalculatorTest {
 
         main.salaryCalculator5.setHoursHospital(10);
 
-        int actual =  main.hospitalSalary(main.salaryCalculator5.getHoursHospital(),main.salaryCalculator5.getHourHospitalSalary());
+        int actual = main.hospitalSalary(main.salaryCalculator5.getHoursHospital(), main.salaryCalculator5.getHourHospitalSalary());
         int expected = 1500;
-        Assert.assertEquals(expected,actual);
-        Assert.assertTrue(actual>1000);
+        Assert.assertEquals(expected, actual);
+        Assert.assertTrue(actual > 1000);
+    }
+
+    @Ignore("I don't have a time")
+    public void isWorkSalaryTest20Thous() {
+
+        boolean actual = main.isWorkSalary(main.salary(main.salaryCalculator0.getSalaryPerHour(), main.salaryCalculator0.getHoursWork(), main.salaryCalculator0.getHoursHospital(), main.salaryCalculator0.getHourHospitalSalary()));
+        boolean expected = true;
+
+        Assert.assertTrue(actual);
+
     }
 
 
